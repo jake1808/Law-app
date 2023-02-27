f266983c01f5d31bb455b35f177c4fdf9681d830
jake1808 <mutale1808@gmail.com>
2023-02-27 13:23:08 +0200

    removed imports

--- src/components/organisms/Legal/LegalSection.tsx
+++ src/components/organisms/Legal/LegalSection.tsx
@@ -1,9 +1,5 @@
-import { ImageCard } from '@/components/atoms/ImageCard/ImageCard'
-import Link from 'next/link'
 import React from 'react'
-import { GoLightBulb } from 'react-icons/go'
-import { BsShieldSlash } from 'react-icons/bs'
-import { TbScribble } from 'react-icons/tb'
+
 import { ForYourBusiness } from '@/components/molecules/LegalMolecules/ForYourBusiness'
 import { ForYouAndYourFamily } from '@/components/molecules/LegalMolecules/ForYouAndYourFamily'
 import { ForYourIntellectualProperty } from '@/components/molecules/LegalMolecules/ForYourIntellectualProperty'
