6f955b3d0852fb6b6ad549bcc62b59066f2d7718
jake1808 <mutale1808@gmail.com>
2023-02-27 10:54:27 +0200

    added props to card image

--- src/components/molecules/ImageCard/ImageCard.tsx
+++ src/components/molecules/ImageCard/ImageCard.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import Image from 'next/image'
-import { GoLightBulb } from 'react-icons/go'
+
+
+interface IconTypeProps {
+    size: number,
+    color:string
+}
 
 interface Props {
     image: {
         src: string,
         alt: string
     },
+    cardText: string,
+    icon: any,
     cardPos: string
 }
 
-export const ImageCard = ({ image, cardPos }: Props) => {
+export const ImageCard = ({ image, cardPos, cardText, icon }: Props) => {
     return (
         <div className='container  mb-6'>
             <div className='relative'>
@@ -22,8 +29,8 @@ export const ImageCard = ({ image, cardPos }: Props) => {
                         height={279.16}
                     />}
                 <div className={`container shadow-2xl w-[80%] z-100 flex flex-col absolute bg-white ${cardPos === 'right' ? 'right-0 top-48' : 'left-0 top-48'} p-6`} >
-                    <GoLightBulb color='black' size={30} className='mb-4'/>
-                      <p className='font-bold'>We&apos;ve made dreams a reality for 2M&#43; entrepreneurs.</p>  
+                    {icon}
+                    <p className='font-bold'>{cardText}</p>
                 </div>
             </div>
         </div>
--- src/components/organisms/Legal/LegalSection.tsx
+++ src/components/organisms/Legal/LegalSection.tsx
@@ -1,6 +1,8 @@
 import { ImageCard } from '@/components/molecules/ImageCard/ImageCard'
 import Link from 'next/link'
 import React from 'react'
+import { GoLightBulb } from 'react-icons/go'
+import {BsShieldSlash} from 'react-icons/bs'
 
 export const LegalSection = () => {
   return (
@@ -10,14 +12,38 @@ export const LegalSection = () => {
         <ImageCard image={{
           src: 'https://source.unsplash.com/random/?carpenter',
           alt: 'ooouu'
-        }} cardPos='right' />
+        }}
+          cardPos='right'
+          cardText='We&apos;ve made dreams a reality for 2M&#43; entrepreneurs.'
+          icon={<GoLightBulb color='black' size={30} className='mb-4' />}
+        />
       </div>
       <div className='container mb-6'>
         <h3 className='font-bold text-2xl mb-4'>For your business</h3>
         <p className='font-semibold text-lg'>Whether you need to form a company&#44; apply for licenses and permits&#44; or get a tax ID&#44; we&#39;ll set your business up for success.</p>
       </div>
-      <div className='container'>
-        <Link href='#'>Start my business</Link>
+      <div className='container mb-12'>
+        <Link href='#' className='btn btn-accent btn-wide'>Start my business</Link>
+        <div className='grid grid-cols-2 gap-4 mt-6'>
+          <Link href='#' className='btn btn-accent btn-outline '>LCC</Link>
+          <Link href='#' className='btn btn-accent btn-outline '>Corporation</Link>
+          <Link href='#' className='btn btn-accent btn-outline '>DBA</Link>
+          <Link href='#' className='btn btn-accent btn-outline '>Get legal help</Link>
+        </div>
+      </div>
+      <div className='container mb-12'>
+        <ImageCard
+          image={{
+            src: 'https://source.unsplash.com/random/?Father',
+            alt: 'ooouu'
+          }}
+          cardPos='left'
+          cardText='Every 2 minutes someone created an estate plan document with us to protect their family&#39;s future.*'
+          icon={<BsShieldSlash color='black' size={30} className='mb-4' />}
+        />
+      
+        
+        
       </div>
     </div>
   )
