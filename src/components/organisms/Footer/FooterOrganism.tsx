d9f1694df0a389bbaa71c9186c50fc75329d236c
jake1808 <mutale1808@gmail.com>
2023-02-28 15:53:17 +0200

    collapse footer menu done

--- src/components/organisms/Footer/FooterOrganism.tsx
+++ src/components/organisms/Footer/FooterOrganism.tsx
@@ -3,6 +3,7 @@ import { SlSocialFacebook, SlSocialInstagram, SlSocialLinkedin, SlSocialTwitter 
 import { SiWhatsapp } from 'react-icons/si'
 import { RxCaretUp } from 'react-icons/rx'
 import { NewsLetterMolecule } from '@/components/molecules/NewsLetter/NewsLetterMolecule'
+import Link from 'next/link'
 
 
 const FooterOrganism = () => {
@@ -17,8 +18,66 @@ const FooterOrganism = () => {
       <div className='flex flex-col items-center mb-8 p-10 mt-8'>
         <h1 className='text-center text-xl font-bold p-12'>Get helpful tips and info from our newsletter!</h1>
         <NewsLetterMolecule />
+        <Link href='' className='mt-10'>
+          See the latest issue
+        </Link>
       </div>
-      <div className='footer mb-6 p-10'>
+      <div className="footer mb-6 p-4 w-full">
+        <div className="collapse collapse-arrow border-y border-base-300 w-full">
+          <input type="checkbox" />
+          <div className="collapse-title text-xl font-medium">
+            Company
+          </div>
+          <div className="collapse-content">
+            <ul>
+              <li className='mb-6'><Link href=''>About</Link></li>
+              <li className='mb-6'><Link href=''>Careers</Link></li>
+              <li className='mb-6'><Link href=''>Contact</Link></li>
+              <li className='mb-6'><Link href=''>Investors</Link></li>
+              <li className='mb-6'><Link href=''>Press</Link></li>
+              <li className='mb-6'><Link href=''>Affiliates</Link></li>
+              <li className='mb-6'><Link href=''>Social Impact</Link></li>
+            </ul>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow border-y border-base-300 w-full">
+          <input type="checkbox" />
+          <div className="collapse-title text-xl font-medium">
+            Support
+          </div>
+          <div className="collapse-content">
+            <ul>
+              <li className='mb-6'><Link href=''>Order Status</Link></li>
+              <li className='mb-6'><Link href=''>Customer Care</Link></li>
+              <li className='mb-6'><Link href=''>Speak with an Attorney</Link></li>
+              <li className='mb-6'><Link href=''>Join our Attorney Network</Link></li>
+              <li className='mb-6'><Link href=''>Security</Link></li>
+              <li className='mb-6'><Link href=''>See all services</Link></li>
+            </ul>
+          </div>
+        </div>
+        <div className="collapse collapse-arrow border-y border-base-300 w-full">
+          <input type="checkbox" />
+          <div className="collapse-title text-xl font-medium">
+            Learn more
+          </div>
+          <div className="collapse-content">
+            <ul>
+              <li className='mb-6'><Link href=''>Legal Help Articles</Link></li>
+              <li className='mb-6'><Link href=''>Legal Forms</Link></li>
+            </ul>
+          </div>
+        </div>
+        <h2 className='text-xl font-bold'>Law App</h2>
+          <div className="flex">
+            <a className="btn btn-sm btn-ghost" href='#'><SlSocialFacebook /></a>
+            <a className="btn btn-sm btn-ghost" href='#'><SlSocialInstagram /></a>
+            <a className="btn btn-sm btn-ghost" href='#'><SlSocialLinkedin /></a>
+            <a className="btn btn-sm btn-ghost" href='#'><SlSocialTwitter /></a>
+            <a className="btn btn-sm btn-ghost" href='#'><SiWhatsapp /></a>
+          </div>
+      </div>
+      <div className='footer mb-6 p-10 hidden'>
         <ul>
           <li><span className="footer-title">Services</span></li>
           <li><a className="link link-hover">Branding</a></li>
